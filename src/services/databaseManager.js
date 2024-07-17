946f1378d8bb20f83d486663311e59f35e2d27c1
NadavMozeson <nadavson02@gmail.com>
2024-07-17 14:01:29 +0300

    Added error handling

--- src/services/databaseManager.js
+++ src/services/databaseManager.js
@@ -1,56 +1,97 @@
 require('dotenv').config()
+require('pretty-error').start()
 const { MongoClient } = require('mongodb')
+
 
 class DBManagerClass {
     constructor() {
-        if (DBManagerClass.instance) {
-            return DBManagerClass.instance
-        }
-        this.client = new MongoClient(process.env.MONGO_CONNECTION_STRING)
-        this.connect()
-        DBManagerClass.instance = this
+        try{
+            if (DBManagerClass.instance) {
+                return DBManagerClass.instance
+            }
+            this.client = new MongoClient(process.env.MONGO_CONNECTION_STRING)
+            this.connect()
+            DBManagerClass.instance = this
+        }catch(err){
+            console.error(err)
+            throw err
+        }        
     }
 
     async connect() {
-        await this.client.connect()
-        this.db = this.client.db('MedHub')
-        console.log(`Connected to "${this.db.databaseName}" database with MongoDB`)
+        try {
+            await this.client.connect()
+            this.db = this.client.db('MedHub')
+            console.log(`Connected to "${this.db.databaseName}" database with MongoDB`)
+        }catch(err) {
+            console.error(err)
+            throw err
+        }
     }
 
     async disconnect() {
-        await this.client.close()
+        try{
+            await this.client.close()
+        }catch(err) {
+            console.error(err)
+            throw err
+        }
     }
 
     async insertOne(collectionName, document) {
-        const collection = this.db.collection(collectionName)
-        const result = await collection.insertOne(document)
-        return result
+        try{
+            const collection = this.db.collection(collectionName)
+            const result = await collection.insertOne(document)
+            return result
+        }catch(err) {
+            console.error(err)
+            throw err
+        }
     }
 
     async findOne(collectionName, query) {
-        const collection = this.db.collection(collectionName)
-        const result = await collection.findOne(query)
-        return result
+        try{
+            const collection = this.db.collection(collectionName)
+            const result = await collection.findOne(query)
+            return result
+        }catch(err) {
+            console.error(err)
+            throw err
+        }
     }
 
     async updateOne(collectionName, query, update) {
-        const collection = this.db.collection(collectionName)
-        const result = await collection.updateOne(query, update)
-        return result
+        try{
+            const collection = this.db.collection(collectionName)
+            const result = await collection.updateOne(query, update)
+            return result
+        }catch(err) {
+            console.error(err)
+            throw err
+        }
     }
 
     async deleteOne(collectionName, query) {
-        const collection = this.db.collection(collectionName)
-        const result = await collection.deleteOne(query)
-        return result
+        try{
+            const collection = this.db.collection(collectionName)
+            const result = await collection.deleteOne(query)
+            return result
+        }catch(err) {
+            console.error(err)
+            throw err
+        }
     }
 
     async checkLogin(inputUsername, inputPassword) {
-        const findResult = await this.findOne('Users', { username: inputUsername, password: inputPassword } )
-        if (findResult){
-            const result = Object.assign( { 'status': 'success' } , findResult)
-            return result
-        } else {
+        try{
+            const findResult = await this.findOne('Users', { username: inputUsername, password: inputPassword } )
+            if (findResult){
+                return { 'status': 'successful', 'data': { 'userID': findResult['_id'], 'username': findResult['username'] } }
+            } else {
+                return { 'status': 'rejected' }
+            }
+        }catch(err) {
+            console.error(err)
             return { 'status': 'failed' }
         }
     }
