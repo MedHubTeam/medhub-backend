f64e452fbbbbd301a37325d68b06a54687c7c35d
NadavMozeson <nadavson02@gmail.com>
2024-08-07 13:38:38 +0300

    Setup basic WebSocket service

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const { port } = require('./config/config.js')
+const { setupWebSocket } = require('./services/webSocket.js')
 
 const app = express()
 
-app.use(cors())
+app.use(cors());
 app.use(express.json())
-
 
 // Load routes
 require('./listenToRoutes')(app)
 
 if (process.env.NODE_ENV !== 'test') {
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`)
-    })
+  const server = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+  })
+
+  // Setup WebSocket server
+  setupWebSocket(server)
 }
 
 module.exports = app
