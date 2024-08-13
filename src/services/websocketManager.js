178fd89ac04da7f0b4abce8a008da2c92202dfb9
TopazN <topaznatan@gmail.com>
2024-08-13 15:39:53 +0300

    Added member count websocket

--- src/services/websocketManager.js
+++ src/services/websocketManager.js
@@ -4,23 +4,36 @@ class WebSocketClass {
     constructor () {
         this.wss = null
         this.userConnections = new Map()
+        this.memberCountConnections = new Set() 
     }
 
     setup (server) {
         this.wss = new WebSocket.Server({ server })
-        this.userConnections = new Map()
 
         this.wss.on('connection', (ws, request) => {
             const url = new URL(request.url, `http://${request.headers.host}`)
             const userId = url.searchParams.get('userId')
             const chatId = url.searchParams.get('chatId')
-            const key = `${userId}:${chatId}`
+            const purpose = url.searchParams.get('purpose')
 
-            this.userConnections.set(key, ws)
+            if (purpose === 'memberCount') {
+                this.memberCountConnections.add(ws)
+                this.broadcastMemberCount()
 
-            ws.on('close', () => {
-                this.userConnections.delete(key)
-            })
+                ws.on('close', () => {
+                    this.memberCountConnections.delete(ws)
+                    this.broadcastMemberCount()
+                })
+            } else if (userId && chatId) {
+                const key = `${userId}:${chatId}`
+                this.userConnections.set(key, ws)
+
+                ws.on('close', () => {
+                    this.userConnections.delete(key)
+                })
+            } else {
+                ws.close()
+            }
         })
     }
 
@@ -39,6 +52,14 @@ class WebSocketClass {
 
     async newChatMessage (chatID, messageID) {
         this.sendMessageToChat (chatID, { chat_id: chatID, message_id: messageID })
+    }
+
+    broadcastMemberCount() {
+        this.memberCountConnections.forEach(ws => {
+            if (ws.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify({ type: 'memberCount', count: this.memberCountConnections.size }))
+            }
+        })
     }
 }
 
